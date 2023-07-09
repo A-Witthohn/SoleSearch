3dbe0b790326251a5e6ed29c869f2cd1442d59ef
Danial Mahmoudi <danialmahmoudi@danials-air.attlocal.net>
2023-07-09 14:18:28 -0400

    updated Home.js page and added Adidas Yeezy Boost png image

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,73 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
+import  { useMutation } from '@apollo/client';
+import { QUERY_SHOES } from '../utils/queries';
+import { LIKE_SHOE } from '../utils/mutations';
+import { REMOVE_SHOE } from '../utils/mutations';
+
+import Auth from '../utils/auth';
+
 
 const Home = () => {
+const { loading, data } = useQuery(QUERY_SHOES);
+const [likeShoe] = useMutation(LIKE_SHOE);
+const [removeShoe] = useMutation(REMOVE_SHOE);
 
+const handleLikeShoe = async (shoeId) => {
+  const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+  if (!token) {
+    return false;
+  }
+
+  try {
+    await likeShoe({
+      variables: { shoeId: shoeId },
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const handleRemoveShoe = async (shoeId) => {
+  const token = Auth.loggedIn() ? Auth.getToken() : null;
   
+  if (!token) {
+    return false;
+  }
+
+  try {
+    await removeShoe({
+      variables: { shoeId: shoeId },
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (loading) {
+  return <div>Loading...</div>;
+}
+
+const { shoes } = data;
+
     return (
       <main>
-        <div className="">
-         <h1>Working</h1>
+        <div>
+         <h1>Rate Your Shoes and Share Your Sole Experience!</h1>
+         {shoes.map((shoe) => (
+            <div key={shoe._id}>
+              <h2>{shoe.shoeName}</h2>
+              <img src={require(`../Assets-SoleSearch/${shoe.image}`).default} alt={shoe.shoeName} />
+              <button onClick={() => handleLikeShoe(shoe._id)}>Like</button>
+              <button onClick={() => handleRemoveShoe(shoe._id)}>Remove</button>
+              <p>Likes: {shoe.likeCount}</p>
+        </div>
+      ))}
         </div>
       </main>
     );
   };
 
 export default Home;
+
