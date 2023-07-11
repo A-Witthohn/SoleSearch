9d6403d84b7671535960e643e4f076e03ec7f8a5
Aaron <awitthohn@yahoo.com>
2023-07-11 18:57:31 -0400

    added styling, routes, images

--- client/src/components/Footer/socialFollow.js
+++ client/src/components/Footer/socialFollow.js
@@ -1,34 +1,66 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-faFacebook,
-faTwitter,
-faInstagram,
-faYoutube
-}
-from "@fortawesome/free-brands-svg-icons"
-export default function SocialFollow() {
-    return (
-      <div className="social-container">
-        <p className="follow-text">Follow us for latest shoes @SoleSearch ❤️</p>
-      {/* <p>Hello</p> */}
-        <div className="social-icons">
-          <a href="https://www.Facebook.com" className="facebook social">
-            <FontAwesomeIcon icon={faFacebook} size="3x" />
-          </a>
-  
-          <a href="https://www.Twitter.com" className="twitter social">
-            <FontAwesomeIcon icon={faTwitter} size="3x" />
-          </a>
-  
-          <a href="https://www.Instagram.com" className="instagram social">
-            <FontAwesomeIcon icon={faInstagram} size="3x" />
-          </a>
-  
-          <a href="https://www.Youtube.com" className="youtube social">
-            <FontAwesomeIcon icon={faYoutube} size="3x" />
-          </a>
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+const SocialFollow = () => {
+  const [showSocialContainer, setShowSocialContainer] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollHeight = document.documentElement.scrollHeight;
+      const clientHeight = window.innerHeight;
+      const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+      // Calculate the distance from the bottom of the visible content area
+      const distanceFromBottom = scrollHeight - (scrollPosition + clientHeight);
+
+      // Set the threshold to a percentage of the clientHeight
+      const threshold = clientHeight * 0.2;
+
+      // Show or hide the social container based on the distance from the bottom
+      setShowSocialContainer(distanceFromBottom <= threshold);
+
+      // Add or remove the loading state based on scroll position
+      if (scrollPosition === 0) {
+        setIsLoading(true);
+      } else {
+        setIsLoading(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      {!isLoading && (
+        <div className={`social-container${showSocialContainer ? ' active' : ''}`}>
+          <p className="follow-text">Follow us for latest shoes @SoleSearch ❤️</p>
+          <div className="social-icons">
+            <a href="https://www.Facebook.com" className="facebook social">
+              <FontAwesomeIcon icon={faFacebook} size="3x" />
+            </a>
+
+            <a href="https://www.Twitter.com" className="twitter social">
+              <FontAwesomeIcon icon={faTwitter} size="3x" />
+            </a>
+
+            <a href="https://www.Instagram.com" className="instagram social">
+              <FontAwesomeIcon icon={faInstagram} size="3x" />
+            </a>
+
+            <a href="https://www.Youtube.com" className="youtube social">
+              <FontAwesomeIcon icon={faYoutube} size="3x" />
+            </a>
+          </div>
         </div>
-      </div>
-    );
-  }
+      )}
+    </>
+  );
+};
+
+export default SocialFollow;
